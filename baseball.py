735ef4f079c6e6d77227d83b9f9ea75e1d124869
junhwik <wjdrhdsbrlfl@gmail.com>
2025-07-31 14:04:31 +0900

    [feature] p.31

--- baseball.py
+++ baseball.py
@@ -8,4 +8,7 @@ class BaseBall:
     def guess(self, guess_number):
         if not guess_number:
             raise TypeError("입력값이 없습니다.")
+        if len(guess_number) !=3:
+            raise TypeError("3자리 숫자 입력")
+
         pass
--- test_baseball.py
+++ test_baseball.py
@@ -1,9 +1,14 @@
 import pytest
 from baseball import BaseBall
 
+@pytest.fixture
 def baseball():
     return BaseBall()
 
 def test_baseball_none_input(baseball):
     with pytest.raises(TypeError):
         baseball.guess(None)
+
+def test_baseball_not_three_digit(baseball):
+    with pytest.raises(TypeError):
+        baseball.guess('12')
